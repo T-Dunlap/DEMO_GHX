4bf7eebe9120a716c8623fb8588638acecdf38bf
Taylor Dunlap <taylor.dunlap@dbtlabs.com>
2023-04-17 21:49:22 +0000

    causing a failure

--- tests/stg_poh_failures_datatype_check.sql
+++ tests/stg_poh_failures_datatype_check.sql
@@ -1,5 +1,5 @@
 
-{{ config(severity = 'warn') }}
+{{ config(severity = 'error') }}
 
 --could also separate these out into different tests to return the results of each system
 --would want to create this with severity = 'error' if the stg_poh_failures was not filtered out in stg_poh
